b41b9b6a052015f4c53903dd0e13c8578aa31aaf
ADSM <yesform2018@gmail.com>
2022-05-22 03:10:30 +0600

    Banner is  completed

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -7,18 +7,54 @@ import slide3 from '../../Assests/Slide/slide3.jpg';
 const Banner = () => {
     return (
         <div>
-            <div class="carousel w-full h-[300px]">
+            <div class="carousel w-full">
                 <div id="item1" class="carousel-item w-full">
-                    <img src={slide} class="w-full" />
+                    <div class="hero min-h-screen bg-base-200">
+                        <div class="hero-content flex-col lg:flex-row">
+                            <img src={slide2} class="max-w-sm rounded-lg shadow-2xl" />
+                            <div>
+                                <h1 class="text-5xl font-bold">Big Choice Of Plumbing Products!</h1>
+                                <p class="py-6">We provide best qualitis products. An ever, We ar most trusted in this world Provident cupiditate voluptatem et in. exercitationem quasi.</p>
+                                <button class="btn btn-primary">Get Started</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div id="item2" class="carousel-item w-full">
-                    <img src={slide1} class="w-full" />
+                    <div class="hero min-h-screen bg-base-200">
+                        <div class="hero-content flex-col lg:flex-row">
+                            <img src={slide} class="max-w-sm rounded-lg shadow-2xl" />
+                            <div>
+                                <h1 class="text-5xl font-bold">Scrowdrivers Proffessional Tools!</h1>
+                                <p class="py-6">We provide best qualitis products. An ever, We ar most trusted in this world Provident cupiditate voluptatem et in. exercitationem quasi.</p>
+                                <button class="btn btn-primary">Get Started</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div id="item3" class="carousel-item w-full">
-                    <img src={slide2} class="w-full" />
+                    <div class="hero min-h-screen bg-base-200">
+                        <div class="hero-content flex-col lg:flex-row">
+                            <img src={slide3} class="max-w-sm rounded-lg shadow-2xl" />
+                            <div>
+                                <h1 class="text-5xl font-bold">Big Choice Of Plumbing Products!</h1>
+                                <p class="py-6">We provide best qualitis products. An ever, We ar most trusted in this world Provident cupiditate voluptatem et in. exercitationem quasi.</p>
+                                <button class="btn btn-primary">Get Started</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div id="item4" class="carousel-item w-full">
-                    <img src={slide3} class="w-full" />
+                    <div class="hero min-h-screen bg-base-200">
+                        <div class="hero-content flex-col lg:flex-row">
+                            <img src={slide1} class="max-w-sm rounded-lg shadow-2xl" />
+                            <div>
+                                <h1 class="text-5xl font-bold">Scrowdrivers Proffessional Tools!</h1>
+                                <p class="py-6">We provide best qualitis products. An ever, We ar most trusted in this world Provident cupiditate voluptatem et in. exercitationem quasi.</p>
+                                <button class="btn btn-primary">Get Started</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="flex justify-center w-full py-2 gap-2">
