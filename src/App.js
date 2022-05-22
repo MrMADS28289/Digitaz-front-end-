8a29b76a84cedc1ff0eda1041c7c1f4102566df8
ADSM <yesform2018@gmail.com>
2022-05-22 16:44:35 +0600

    Dark mood and drower navbar is completed

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import { HelmetProvider } from 'react-helmet-async';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import NavBar from './Pages/Shared/NavBar';
+import NotFound from './Pages/Shared/NotFound';
+import { themeChange } from 'theme-change'
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    themeChange(false)
+  }, [])
+
   return (
     <HelmetProvider>
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/home' element={<Home />} />
-      </Routes>
+      <NavBar >
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </NavBar>
     </HelmetProvider>
   );
 }
